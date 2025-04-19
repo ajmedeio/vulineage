f13bc582aff577960a4ebdd8872d66cba823e905
Alexander Medeiros <ajmed13@gmail.com>
2025-04-19 11:54:22 -0400

    Add certbot service configuration and update example script for timer setup

--- server/etc/systemd/system/example.sh
+++ server/etc/systemd/system/example.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# These commands are roughly by memory so there might be some troubleshooting if we need to setup the server from scratch.
+
+# after installing certbot and placing the files in their respective directories, enable the certbot timer to keep the certificates up to date
+sudo systemctl enable certbot.timer
+
 # installing certbot on the server
 sudo su
 yum install pip nginx -y
@@ -7,6 +13,3 @@ cd /opt/certbot/bin
 ./pip install certbot certbot-nginx
 . ./activate
 ./certbot run
-
-# after installing certbot and placing the files in their respective directories, enable the certbot timer to keep the certificates up to date
-sudo systemctl enable certbot.timer
